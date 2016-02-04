a22053bf212229a8f290f480a301fc389dad170b
marvin-2016 <jzrna@srce.hr>
2016-02-04 15:54:25 +0100

    created update scripts

---
+++ install/cdr-stats-update-functions.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+#
+# CDR-Stats License
+# http://www.cdr-stats.org
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (C) 2011-2015 Star2Billing S.L.
+#
+# The Initial Developer of the Original Code is
+# Arezqui Belaid <info@star2billing.com>
+
+BRANCH='develop'
+
+#Install mode can me either CLONE or DOWNLOAD
+
+INSTALL_DIR='/usr/share/cdrstats'
+CDRSTATS_ENV="cdr-stats"
+SCRIPT_NOTICE="This install script is only intended to run on Debian 7.X"
+
+#Django bug https://code.djangoproject.com/ticket/16017
+export LANG="en_US.UTF-8"
+
+# Identify Linux Distribution
+func_identify_os() {
+    if [ -f /etc/debian_version ] ; then
+        DIST='DEBIAN'
+        if [ "$(lsb_release -cs)" != "wheezy" ] && [ "$(lsb_release -cs)" != "jessie" ]; then
+            echo $SCRIPT_NOTICE
+            exit 255
+        fi
+        DEBIANCODE=$(lsb_release -cs)
+    elif [ -f /etc/redhat-release ] ; then
+        DIST='CENTOS'
+        if [ "$(awk '{print $3}' /etc/redhat-release)" != "6.2" ] && [ "$(awk '{print $3}' /etc/redhat-release)" != "6.3" ] && [ "$(awk '{print $3}' /etc/redhat-release)" != "6.4" ] && [ "$(awk '{print $3}' /etc/redhat-release)" != "6.5" ]; then
+            echo $SCRIPT_NOTICE
+            exit 255
+        fi
+    else
+        echo $SCRIPT_NOTICE
+        exit 1
+    fi
+}
+
+#Fuction to create the virtual env
+func_activate_virtualenv() {
+    #Prepare settings for installation
+    case $DIST in
+        'DEBIAN')
+            SCRIPT_VIRTUALENVWRAPPER="/usr/share/virtualenvwrapper/virtualenvwrapper.sh"
+        ;;
+        'CENTOS')
+            SCRIPT_VIRTUALENVWRAPPER="/usr/bin/virtualenvwrapper.sh"
+        ;;
+    esac
+
+    # Setup virtualenv
+    export WORKON_HOME=/usr/share/virtualenvs
+    source $SCRIPT_VIRTUALENVWRAPPER
+
+    workon $CDRSTATS_ENV
+
+    echo "Virtualenv $CDRSTATS_ENV activated"
+}
+
+#Function to install the code source
+func_update_source(){
+
+    #get CDR-Stats
+    echo "Update CDR-Stats..."
+    cd /usr/src/
+    rm -rf cdr-stats
+    mkdir /var/log/cdr-stats
+
+    git clone -b $BRANCH git://github.com/marvin-2016/cdr-stats.git
+    cd cdr-stats
+
+    #Install Develop / Master
+    if echo $BRANCH | grep -i "^develop" > /dev/null ; then
+        git checkout -b develop --track origin/develop
+    fi
+
+    # Copy files
+    cp -r /usr/src/cdr-stats/cdr_stats $INSTALL_DIR
+}
+
+#Function to prepare settings_local.py
+func_backup_settings(){
+    #Copy settings_local.py into tmp dir
+    cp $INSTALL_DIR/cdr_stats/settings_local.py /tmp/settings_local.py
+}
+
+#Function to prepare settings_local.py
+func_backup_settings(){
+    #Copy settings_local.py from tmp dir
+    mv /tmp/settings_local.py $INSTALL_DIR/cdr_stats/settings_local.py
+}
+
+#Update Django CDR-stats
+func_django_cdrstats_update(){
+    cd $INSTALL_DIR/
+    python manage.py syncdb --noinput
+    python manage.py migrate
+
+    clear
+    echo ""
+    echo "Collects the static files"
+    python manage.py collectstatic --noinput
+
+    #Load Countries Dialcode
+    python manage.py load_country_dialcode
+
+    #Load default gateways & billing data
+    python manage.py loaddata voip_gateway/fixtures/voip_gateway.json
+    python manage.py loaddata voip_gateway/fixtures/voip_provider.json
+    python manage.py load_sample_voip_billing
+}
---
+++ install/update-cdr-stats.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+# CDR-Stats License
+# http://www.cdr-stats.org
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (C) 2011-2015 Star2Billing S.L.
+#
+# The Initial Developer of the Original Code is
+# Arezqui Belaid <info@star2billing.com>
+#
+
+#
+# To download and run the script on your server :
+#
+# cd /usr/src/ ; rm update-cdr-stats.sh ; wget --no-check-certificate https://raw.github.com/marvin-2016/cdr-stats/master/install/update-cdr-stats.sh -O update-cdr-stats.sh ; bash update-cdr-stats.sh
+#
+# Install develop branch
+# cd /usr/src/ ; rm update-cdr-stats.sh ; wget --no-check-certificate https://raw.github.com/marvin-2016/cdr-stats/develop/install/update-cdr-stats.sh -O update-cdr-stats.sh ; bash update-cdr-stats.sh
+#
+
+BRANCH='develop'
+
+#Get Scripts dependencies
+cd /usr/src/
+rm cdr-stats-functions.sh
+wget --no-check-certificate https://raw.github.com/marvin-2016/cdr-stats/$BRANCH/install/cdr-stats-functions.sh -O cdr-stats-functions.sh
+
+#Include cdr-stats update functions
+source cdr-stats-update-functions.sh
+
+
+#Identify the OS
+func_identify_os
+
+echo "========================================================================="
+echo ""
+echo "CDR-Stats update will start now!"
+echo ""
+echo "Press Enter to continue or CTRL-C to exit"
+echo ""
+read INPUT
+
+func_activate_virtualenv
+func_backup_settings
+func_update_source
+func_restore_settings
+func_django_cdrstats_update
+
+clear
+echo ""
+echo "Congratulations, CDR-Stats is now updated!"
+echo "--------------------------------------------"
+echo ""
+echo "Thank you for installing CDR-Stats"
+echo "Yours,"
+echo "The Star2Billing Team"
+echo "http://www.star2billing.com and http://www.cdr-stats.org/"
+echo ""
+echo "========================================================================="
+echo ""
